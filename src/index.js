10b1471f3c8909c0f894c390eb86e34c0fd744e0
Ivan Eroshin <itseroshin@gmail.com>
2023-09-28 23:03:46 +0300

    fead: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const str = expr.match(/.{1,10}/g);
+    var morse = '';
+    var res = [];
+
+    for (const char of str) {
+        morse = MORSE_TABLE[convert(char.match(/.{1,2}/g))];
+        if(morse === undefined)
+            morse = ' ';
+        res.push(morse);
+    }
+
+    return res.join('');
+}
+
+function convert(letter) {
+    var morse = '';
+    const filter = letter.filter(x => x !== '00');
+    filter.forEach(x => {
+        morse += x === '10' ? '.' : (x === '11' ? '-' : '_');
+    })
+    return morse;
 }
 
 module.exports = {
